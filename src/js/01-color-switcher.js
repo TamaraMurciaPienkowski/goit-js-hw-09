2c49e771fe056c95b46407c8600433adb7e74e3a
TamaraMurciaPienkowski <tamaramurciapienkowski@gmail.com>
2023-05-07 18:04:29 +0200

    finished task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+let colorInterval = null;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+function startHandler() {
+  const colorChange = getRandomHexColor();
+  document.body.style.backgroundColor = colorChange;
+}
+
+btnStart.addEventListener('click', () => {
+  btnStart.disabled = true;
+  colorInterval = setInterval(() => {
+    startHandler();
+  }, 1000);
+});
+btnStop.addEventListener('click', () => {
+  btnStart.disabled = false;
+  clearInterval(colorInterval);
+});
